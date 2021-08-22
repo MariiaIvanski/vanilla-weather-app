278f7466e0382f01b57f1914aa4371132a6c04b5
mariia-ivanski <mariia.ivanski@gmail.com>
2021-08-22 15:13:23 +0300

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 function displayTemperature(response) {
-  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
 }
 
 const apiKey = "a2d283df905dedf8786b96ad24673f92";
 let weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+
 axios.get(weatherUrl).then(displayTemperature);
