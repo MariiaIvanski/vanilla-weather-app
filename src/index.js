b5810d7d80f88f7c13df544d91c7d475849a99a4
mariia-ivanski <mariia.ivanski@gmail.com>
2021-08-27 18:45:30 +0300

    update forecast in  index.js

--- src/index.js
+++ src/index.js
@@ -21,28 +21,41 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[day];
+}
+
 function displayForecast(response) {
-  console.log(response.data.daily);
+  let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
-  let days = ["Sat", "Sun", "Mon", "Tue", "Wed", "Thu"];
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `
+
+  forecast.forEach(function (forecastDay) {
+    forecastHTML += `
   
-              <div class="col-2">
-                <div class="weather-forecast-date">${day}</div>
+              <div class="col-3">
+                <div class="weather-forecast-date">${formatDay(
+                  forecastDay.dt
+                )}</div>
                 <img
-                  src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
+                  src="http://openweathermap.org/img/wn/${
+                    forecastDay.weather[0].icon
+                  }@2x.png"
                   alt="sun"
                   width="42"
                 />
                 <div class="weather-forecast-description">Clear sky</div>
                 <div class="weather-forecast-temperature">
-                  <span class="weather-forecast-max">18°</span>
-                  <span class="weather-forecast-min">12°</span>
+                  <span class="weather-forecast-max"> ${Math.round(
+                    forecastDay.temp.max
+                  )}°</span>
+                  <span class="weather-forecast-min">${Math.round(
+                    forecastDay.temp.min
+                  )}°</span>
                 </div>
               </div>
             
